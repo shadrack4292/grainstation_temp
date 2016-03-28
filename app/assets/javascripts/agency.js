e702dc1598c4dfe40f8fe27bd3979ebec0e6f115
Edwin Agbenyega <eddybanks03@gmail.com>
2016-03-28 13:28:25 -0600

    styling contact page

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -13,14 +13,14 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    })
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 });
