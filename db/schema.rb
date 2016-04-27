da6f53591a2e8ccb4ebe40c79c318ae325aadde2
Edwin Agbenyega <eddybanks03@gmail.com>
2016-04-27 13:15:22 -0600

    Change menu design to carousel and ontap design to modals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422193244) do
+ActiveRecord::Schema.define(version: 20160427181332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160422193244) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "img_url"
   end
 
   create_table "taps", force: :cascade do |t|
@@ -40,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160422193244) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "img_url"
   end
 
 end
