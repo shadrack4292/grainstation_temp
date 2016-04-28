97b008c217a644c09668ab168e6f86327c6bca58
Edwin Agbenyega <eddybanks03@gmail.com>
2016-04-28 14:17:42 -0600

    added full list menu modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,7 @@ $(document).ready(function(){
   document.getElementById('vid1').addEventListener('loadedmetadata', function() {
     this.currentTime = 40;
   }, false);
+  $('#menuModal').on('shown.bs.modal', function () {
+    $('.modal-title').focus()
+  });
 });
